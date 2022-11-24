3590f660d7cc54e149d45ef5960fc9d5d7f95852
paulovictor11 <pauloalmeidajp652@gmail.com>
2022-11-24 15:23:22 -0300

    Movendo carregando de fontes do google para lib interna do nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
+import { Roboto } from "@next/font/google";
 import "../styles/global.css";
 
+const roboto = Roboto({ weight: ["400", "700"] });
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <style jsx global>{`
+                html {
+                    font-family: ${roboto.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
